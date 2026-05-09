86c6a6753172ca3b88ba6bf3b7dbd8a4e00093bf
Sanarazzaq78 <razzaqsana@gmail.com>
2026-05-09 11:50:14 +0500

    i can run to my calculator

--- operation.py
+++ operation.py
@@ -1,20 +1,21 @@
+"""Files to stores Mathematics basic operation."""
+
+
 class Operation:
-    def __init__(self,operation):
-        self.operation=operation
+    """Basic Mathematical Operation class."""
+    def __init__(self,number1: int ,number2:int ) -> None:
+        """Initializer."""
+        self.number1=number1
+        self.number2=number2
 
-    def addition(self,number1,number2):
-        return number1+number2
-    def subtraction(self,number1,number2):
-        return number1 + number2
-    def multiplication(self,number1,number2):
-        return number1 + number2
-    def division(self,number1,number2):
-        return number1/number2
-    def modulus(self,number1,number2):
-        return number1 % number2
+    def add(self):
+      print("add operation",self.number1+self.number2)
 
-
-
-
-
-
+    def sub(self):
+        print("sub operation", self.number1- self.number2)
+    def mul(self):
+        print("mul operation", self.number1* self.number2)
+    def div(self):
+        print("div operation", self.number1/ self.number2)
+    def modu(self):
+        print("modu operation", self.number1 % self.number2)
